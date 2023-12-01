04e5aa6e0d472737e4191286435e2ada6055c060
Peter Loyd <loydp@gmail.com>
2023-11-30 21:53:32 -0800

    bones of day1, done at work

---
+++ 2023/day1/day1.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <string>
+
+vector<std::string> get_input(std::string ) {
+	return 0;
+}
+
+int sol_1(std::vector<std::string> input) {
+	return 0;
+}
+
+std::string SAMPLE = "sample.dat";
+std::string INPUT = "input.dat";
+
+
+int main() {
+	std::vector<std::string> input = get_input(SAMPLE);
+	int res1 = sol_1(input);
+}

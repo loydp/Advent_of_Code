cbaceadf4dd9d3ef7d17244e480e6932c8f6f02a
loydp <54460504+loydp@users.noreply.github.com>
2025-04-28 20:26:44 -0700

    d2 2024 p2 python don

--- 2024/day2/d2.py
+++ 2024/day2/d2.py
@@ -2,37 +2,64 @@ def get_data(input_path):
     with open(input_path) as f:
         return f.readlines()
 
+def treat(data):
+    ret = []
+    for line in data:
+        line = str.strip(line)
+        line = line.split(" ")
+        ret.append([int(num) for num in line])
+    return ret
 
-input_path = "data.txt"
-
-def treat(line):
-    line = str.strip(line)
-    line = line.split(" ")
-    return [int(num) for num in line]
-
-def check(line):
-    if line[1] > line[0]:
-        for i in range(len(line) - 1):
-            diff = line[i + 1] - line[i]
-            if diff < 1 or diff > 3:
-                return 0
+# confirmed works
+def safe_diff(input_1: int, input_2: int) -> int:
+    """
+    If input 1 is 1 to 3 greater than input 2, return 1
+    If input 1 is 1 to 3 less than input 2, return -1
+    else return 0
+    """
+    if 1 <= (input_2 - input_1) <= 3:
         return 1
-
-    if line[1] < line[0]:
-        for i in range(len(line) - 1):
-            diff = line[i] - line[i + 1]
-            if diff < 1 or diff > 3:
-                return 0
-        return 1
+    if 1 <= (input_1 - input_2) <= 3:
+        return -1
     return 0
 
+def process_1(reactor_levels):
+    total_count = 0
+    for levels in reactor_levels:
+        level_check = []
+        i = 0
+        while i < len(levels) - 1:
+            level_check.append(safe_diff(levels[i], levels[i + 1]))
+            i += 1
+        if all(map(lambda x: x == + 1, level_check)):
+            total_count += 1
+        if all(map(lambda x: x == - 1, level_check)):
+            total_count += 1
+        continue
+    return total_count
+        
 
-def process1(input_path):
-    data = get_data(input_path)
-    data = [treat(line) for line in data]
-    print(data)
-    data = [check(line) for line in data]
-    
-    return sum(data)
+def process_2(reactor_levels):
+    total_count = 0
+    for level in reactor_levels:
+        if process_1([level]):
+            total_count += 1
+        else:
+            i = 0
+            while i < len(level):
+                new_level = level[:i] + level[i + 1:]
+                if process_1([new_level]):
+                    total_count += 1
+                    break
+                i += 1
 
-sol1 = process1(input_path)
+    return total_count
+
+input_path = "data.txt"
+data = get_data(input_path)
+reactor_levels = treat(data)
+sol1 = process_1(reactor_levels)
+sol2 = process_2(reactor_levels)
+
+print(sol1)
+print(sol2)
